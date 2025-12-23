c378d51c2bfb108bd9454f5cf34cf193e98cf896
sinik111 <kangmark95@naver.com>
2025-12-23 21:52:03 +0900

    hdr 셰이더 수정

--- 15_PBR/QuadHDRPS.hlsl
+++ 15_PBR/QuadHDRPS.hlsl
@@ -45,12 +45,9 @@ float4 main(PS_INPUT_QUAD input) : SV_TARGET
     
     float exposureFactor = pow(2.0f, g_exposure);
     C_linear709 *= exposureFactor;
-    
-    float3 C_tonemapped;
-    C_tonemapped = ACESFilm(C_linear709);
-  
+      
     // PQ로 변환 (HDR10 출력용)
-    float3 C_pq = LinearToPQ(C_tonemapped, g_maxHDRNits);
+    float3 C_pq = LinearToPQ(C_linear709, g_maxHDRNits);
     // 최종 PQ 인코딩된 값 [0.0, 1.0]을 R10G10B10A2_UNORM 백버퍼에 출력
     return float4(C_pq, 1.0);
 }
